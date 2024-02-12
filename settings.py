f20e1bee1182044c3882af69e696ebb4a3e98e3e
Danil <86056760+comelovedo@users.noreply.github.com>
2024-02-12 19:44:29 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-valid_email = "Bazucer@gmail.com"
-valid_password = "qwerty555"
+valid_email = "your email"
+valid_password = "your password"
 
 invalid_email = "qwerty@mail.ru"
 invalid_age = "5435346346345234236555523423543634553453245235555"
